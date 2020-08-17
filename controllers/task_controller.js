fb273bcb7f4160dd45d289fb128811d7835de2b6
Aamir <aamirhafiez@gmail.com>
2020-08-18 03:06:58 +0530

    Added functionality to view all tasks and delete tasks

--- controllers/task_controller.js
+++ controllers/task_controller.js
@@ -11,3 +11,26 @@ module.exports.add = function(req, res){
         res.redirect('back');
     });
 }
+
+// deleting task from database
+module.exports.delete = function(req, res){
+    let ids = req.body.task;
+    if(typeof(ids) === 'string'){
+        Task.findByIdAndDelete(ids, function(err, task){
+            if(err){
+                console.log('Error in finding and deleting task');
+                return;
+            }
+            return res.redirect('back');
+        });
+    }else{
+        for(let i of ids){
+            Task.findByIdAndDelete(i, function(err, task){
+                if(err){
+                    return;
+                }
+            });
+        }
+        return res.redirect('back');
+    }
+}
--- routes/task.js
+++ routes/task.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const taskController = require('../controllers/task_controller');
+// adding task
 router.post('/add', taskController.add);
+
+// delete task
+router.post('/delete', taskController.delete);
 
 module.exports = router;
